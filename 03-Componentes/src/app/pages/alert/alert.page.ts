a9e215e7bf195c69cfa66d19018a5b1e9de7a18a
Unknown <matijacob@live.com>
2020-02-03 16:20:40 -0300

    Step 49

--- 03-Componentes/src/app/pages/alert/alert.page.ts
+++ 03-Componentes/src/app/pages/alert/alert.page.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AlertController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-alert',
-  templateUrl: './alert.page.html',
-  styleUrls: ['./alert.page.scss'],
+  selector: "app-alert",
+  templateUrl: "./alert.page.html",
+  styleUrls: ["./alert.page.scss"]
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  titulo: string;
 
-  ngOnInit() {
+  constructor(private alertCtrl: AlertController) {}
+
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      header: "Alert",
+      subHeader: "Subtitle",
+      message: "This is an alert message.",
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          cssClass: "secondary",
+          handler: blah => {
+            console.log("Cancelar");
+          }
+        },
+        {
+          text: "Ok",
+          handler: blah => {
+            console.log("Boton ok");
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
+  async presentTitle() {
+    const input = await this.alertCtrl.create({
+      header: 'Cambiar input',
+      subHeader: 'Ingrese nuevo nombre de ventana',
+      inputs: [
+        {
+          name: 'txtTitulo',
+          type: 'text',
+          placeholder: 'Nombre'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'OK',
+          handler: ( data ) => {
+            console.log('Confirm Ok', data);
+            this.titulo = data.txtTitulo;
+          }
+        }
+      ]
+    });
+
+    await input.present();
+  }
+
+  ngOnInit() {}
 }
--- 03-Componentes/src/app/pages/inicio/inicio.page.ts
+++ 03-Componentes/src/app/pages/inicio/inicio.page.ts
@@ -7,9 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
+  componentes: Componente[]= [
+    {
+      icon: 'american-football',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'appstore',
+      name: 'Alert',
+      redirectTo: '/alert'
+    },
+    {
+      icon: 'beaker',
+      name: 'Avatar',
+      redirectTo: '/avatar'
+    },
+    {
+      icon: 'radio-button-on',
+      name: 'Botones y Router',
+      redirectTo: '/botones'
+    },
+    {
+      icon: 'card',
+      name: 'Cards',
+      redirectTo: '/card'
+    },
+    {
+      icon: 'checkmark-circle-outline',
+      name: 'Checkbox',
+      redirectTo: '/check'
+    }
+
+  ]
+
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+interface Componente{
+  icon: string;
+  name: string;
+  redirectTo: string;
 }
